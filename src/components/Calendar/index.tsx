a5383bb67e29f2cdba629011ab6b9a68521d6e05
Roman K <83346225+samhainsamhainsamhain@users.noreply.github.com>
2023-01-09 20:38:55 +0300

    choose date and time for booking

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,32 +1,67 @@
 import { FC, useState } from "react";
-import ReactCalendar, { DateCallback } from "react-calendar";
+import ReactCalendar from "react-calendar";
+import { add, format } from "date-fns";
+import { CLOSING_HOUR, INTERVAL, OPENING_HOUR } from "../../constants";
 
 type indexProps = {};
 
-type DateType = {
-  date: Date | null;
-  dateTime: Date | null;
+type BookingType = {
+  bookingDate: Date | null;
+  bookingTime: Date | null;
 };
 
 const index: FC<indexProps> = ({}: indexProps) => {
-  const [date, setDate] = useState<DateType>({
-    date: null,
-    dateTime: null,
+  const [date, setDate] = useState<BookingType>({
+    bookingDate: null,
+    bookingTime: null,
   });
 
-  function chooseDate(date: Date) {
-    setDate((prev) => ({ ...prev, dateTime: date }));
+  function getBookingTimes() {
+    if (!date.bookingDate) return;
+
+    const { bookingDate } = date;
+
+    const openTime = add(bookingDate, { hours: OPENING_HOUR });
+    const closeTime = add(bookingDate, { hours: CLOSING_HOUR });
+    const interval = INTERVAL;
+
+    const bookingTimes = [];
+    for (let i = openTime; i <= closeTime; i = add(i, { minutes: interval })) {
+      bookingTimes.push(i);
+    }
+
+    return bookingTimes;
+  }
+
+  const times = getBookingTimes();
+
+  function chooseBookingDate(date: Date) {
+    setDate((prev) => ({ ...prev, bookingDate: date }));
+  }
+
+  function chooseBookingTime(time: Date) {
+    console.log(time);
+
+    setDate((prev) => ({ ...prev, bookingTime: time }));
   }
 
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      {date.date ? (
-        <div>time component</div>
+    <div className="flex flex-col items-center justify-center">
+      {date.bookingDate ? (
+        <div className="flex flex-row items-center justify-center">
+          {times?.map((time, i) => (
+            <div className="m-2 rounded-xl bg-cyan-400 p-2" key={`time-${i}`}>
+              <button onClick={() => chooseBookingTime(time)}>
+                {format(time, "kk:mm")}
+              </button>
+            </div>
+          ))}
+        </div>
       ) : (
         <ReactCalendar
           className="REACT-CALENDAR p-2"
           view="month"
-          onClickDay={chooseDate}
+          onClickDay={chooseBookingDate}
           minDate={new Date()}
           locale="en-GB"
         />
---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+export const OPENING_HOUR = 9; // hours
+export const CLOSING_HOUR = 18; // hours
+export const INTERVAL = 30; // minutes
