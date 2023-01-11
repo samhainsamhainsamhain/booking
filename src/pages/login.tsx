ae5860be5790dd01dbec04f2116f7ab98126477e
Roman K <83346225+samhainsamhainsamhain@users.noreply.github.com>
2023-01-11 10:57:36 +0300

    disable sign in if email or pass is empty

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -73,6 +73,7 @@ const login: FC<loginProps> = ({}: loginProps) => {
                 </a>
               </div>
               <button
+                disabled={!input.email || !input.password}
                 type="submit"
                 className="inline-block w-full rounded bg-blue-600 px-7 py-3 text-sm font-medium uppercase leading-snug text-white shadow-md transition duration-150 ease-in-out hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg"
                 data-mdb-ripple="true"
