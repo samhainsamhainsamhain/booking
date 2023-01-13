e4634604684f5aea3ebf09f05b604e6984fb2f9c
Roman K <83346225+samhainsamhainsamhain@users.noreply.github.com>
2023-01-13 23:04:38 +0300

    fix svg properties of Spinner

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -14,8 +14,8 @@ const Spinner: FC<SpinnerProps> = ({ }: SpinnerProps) => {
       <circle
         className="path"
         fill="none"
-        stroke-width="6"
-        stroke-linecap="round"
+        strokeWidth="6"
+        strokeLinecap="round"
         cx="33"
         cy="33"
         r="30"
