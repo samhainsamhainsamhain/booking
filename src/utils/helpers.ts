1965dc8698e0455eb26b9213b645ccac66a6bd42
Roman K <83346225+samhainsamhainsamhain@users.noreply.github.com>
2023-01-14 17:51:38 +0300

    helper function for categories select

---
+++ src/utils/helpers.ts
@@ -0,0 +1,10 @@
+import { categories } from "src/constants";
+
+function capitalize(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export const selectOptions = categories.map((category) => ({
+  value: category,
+  label: capitalize(category),
+}));
