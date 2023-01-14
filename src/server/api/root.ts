2c9b095ad6f5617ef6123b06fea6328da5755eb3
Roman K <83346225+samhainsamhainsamhain@users.noreply.github.com>
2023-01-14 20:37:15 +0300

    add menuRouter

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter } from "./trpc";
 import { adminRouter } from "./routers/admin";
+import { menuRouter } from "./routers/menu";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { adminRouter } from "./routers/admin";
  */
 export const appRouter = createTRPCRouter({
   admin: adminRouter,
+  menu: menuRouter,
 });
 
 // export type definition of API
