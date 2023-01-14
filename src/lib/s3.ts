e51c26cb5ae2956954ebb59b1839bd5b3a9d96fa
Roman K <83346225+samhainsamhainsamhain@users.noreply.github.com>
2023-01-14 20:35:32 +0300

    create s3 client

---
+++ src/lib/s3.ts
@@ -0,0 +1,8 @@
+import S3 from "aws-sdk/clients/s3";
+
+export const s3 = new S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+  signatureVersion: "v4",
+});
