a0593004fd8e8c92ea6fee52afa40ec65d2380b7
Roman K <83346225+samhainsamhainsamhain@users.noreply.github.com>
2023-01-14 17:49:01 +0300

    max file size and categories constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,7 @@
 export const OPENING_HOUR = 9; // hours
 export const CLOSING_HOUR = 18; // hours
 export const INTERVAL = 30; // minutes
+
+export const MAX_FILE_SIZE = 1024 * 1024 * 5; // 5MB
+
+export const categories = ["all", "breakfast", "lunch", "dinner"] as const;
