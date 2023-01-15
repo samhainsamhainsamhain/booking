e645e6fad3338e05db83d922bce30ce6f7a9eef6
Roman K <83346225+samhainsamhainsamhain@users.noreply.github.com>
2023-01-15 14:08:21 +0300

    fix: firefox auto-fill don't trigger onChange if input has value property

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { ChangeEvent, FC, useState } from "react";
+import { ChangeEvent, FC, useState, useRef, useEffect } from "react";
 import { api } from "../utils/api";
 
 type loginProps = {};
@@ -10,6 +10,19 @@ const login: FC<loginProps> = ({}: loginProps) => {
     email: "",
     password: "",
   });
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (emailRef.current?.value && passwordRef.current?.value)
+        setInput({
+          email: emailRef.current.value,
+          password: passwordRef.current.value,
+        });
+      console.log(emailRef.current?.value, passwordRef.current?.value);
+    }, 300);
+  }, []);
 
   const { mutate: login, isError } = api.admin.login.useMutation({
     onSuccess: () => {
@@ -42,20 +55,20 @@ const login: FC<loginProps> = ({}: loginProps) => {
             <form onSubmit={handleSubmit}>
               <div className="mb-6">
                 <input
+                  ref={emailRef}
                   type="text"
                   name="email"
                   onChange={handleChange}
-                  value={input.email}
                   className="form-control m-0 block w-full rounded border border-solid border-gray-300 bg-white bg-clip-padding px-4 py-2 text-xl font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none"
                   placeholder="Email address"
                 />
               </div>
               <div className="mb-6">
                 <input
+                  ref={passwordRef}
                   type="password"
                   name="password"
                   onChange={handleChange}
-                  value={input.password}
                   className="form-control m-0 block w-full rounded border border-solid border-gray-300 bg-white bg-clip-padding px-4 py-2 text-xl font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none"
                   placeholder="Password"
                 />
