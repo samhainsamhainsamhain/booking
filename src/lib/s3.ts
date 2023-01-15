2b36dc4ec41f157397ad4f381937ff6c07cd07b2
Roman K <83346225+samhainsamhainsamhain@users.noreply.github.com>
2023-01-15 17:32:27 +0300

    rename reserved env variables to deploy for vercel

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,8 +1,8 @@
 import S3 from "aws-sdk/clients/s3";
 
 export const s3 = new S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID_VERCEL,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_VERCEL,
+  region: process.env.AWS_REGION_VERCEL,
   signatureVersion: "v4",
 });
