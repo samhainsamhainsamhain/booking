86f9ca06159f304293bee5e4e68394260faad861
Roman K <83346225+samhainsamhainsamhain@users.noreply.github.com>
2023-01-23 23:27:21 +0300

    menu page for admin

---
+++ src/pages/menu.tsx
@@ -0,0 +1,51 @@
+import Menu from "@components/Menu";
+import Spinner from "@components/Spinner";
+import { parseISO } from "date-fns/esm";
+import { useRouter } from "next/router";
+import { FC, useEffect, useState } from "react";
+import { now } from "src/constants";
+import { api } from "src/utils/api";
+
+type menuProps = {};
+
+const menu: FC<menuProps> = ({}: menuProps) => {
+  const router = useRouter();
+  const [selectedTime, setSelectedTime] = useState<string | null>(null); // as ISO string
+  const { isFetchedAfterMount } = api.menu.checkMenuStatus.useQuery(undefined, {
+    onError: () => {
+      // check for validity of selectedTime failed
+      // TODO Handle error
+    },
+  });
+
+  useEffect(() => {
+    const selectedTime = localStorage.getItem("selectedTime");
+    if (!selectedTime) {
+      router.push("/");
+    } else {
+      const date = parseISO(selectedTime);
+      if (date < now) router.push("/");
+
+      setSelectedTime(selectedTime);
+    }
+  }, []);
+
+  return (
+    <>
+      {isFetchedAfterMount && selectedTime ? (
+        <>
+          <button onClick={() => router.push("/")}>
+            Back to time selection
+          </button>
+          <Menu selectedTime={selectedTime} />
+        </>
+      ) : (
+        <div className="flex h-screen items-center justify-center">
+          <Spinner />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default menu;
