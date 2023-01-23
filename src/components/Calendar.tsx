78f28d2bc44705fb8686940a07ca57dbbd9a43ba
Roman K <83346225+samhainsamhainsamhain@users.noreply.github.com>
2023-01-23 19:23:44 +0300

    disable days in calendar, refactor code

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,33 +1,41 @@
-import { Dispatch, FC, SetStateAction, useState } from "react";
+import { format, formatISO, isBefore, parse } from "date-fns";
+import { FC, useEffect, useState } from "react";
 import ReactCalendar from "react-calendar";
-import { add, format } from "date-fns";
-import { CLOSING_HOUR, INTERVAL, OPENING_HOUR } from "../constants";
+import { useRouter } from "next/router";
+
+import { Day } from "@prisma/client";
 import { DateTime } from "@types";
 
-type indexProps = {
-  date: DateTime;
-  setDate: Dispatch<SetStateAction<DateTime>>;
+import { getOpeningTimes, roundUpToNearestMinutes } from "src/utils/helpers";
+import { INTERVAL, now } from "../constants";
+
+type CalendarProps = {
+  days: Day[];
+  closedDays: string[];
 };
 
-const Calendar: FC<indexProps> = ({ date, setDate }: indexProps) => {
-  function getBookingTimes() {
-    if (!date.bookingDate) return;
+const Calendar: FC<CalendarProps> = ({ days, closedDays }) => {
+  const router = useRouter();
 
-    const { bookingDate } = date;
+  const today = days.find((day) => day.dayOfWeek === now.getDay());
+  const rounded = roundUpToNearestMinutes(now, INTERVAL);
+  const closing = parse(today!.closeTime, "kk:mm", now);
+  const tooLate = !isBefore(rounded, closing);
+  if (tooLate) closedDays.push(formatISO(new Date().setHours(0, 0, 0, 0)));
 
-    const openTime = add(bookingDate, { hours: OPENING_HOUR });
-    const closeTime = add(bookingDate, { hours: CLOSING_HOUR });
-    const interval = INTERVAL;
+  const [date, setDate] = useState<DateTime>({
+    bookingDate: null,
+    bookingTime: null,
+  });
 
-    const bookingTimes = [];
-    for (let i = openTime; i <= closeTime; i = add(i, { minutes: interval })) {
-      bookingTimes.push(i);
+  useEffect(() => {
+    if (date.bookingTime) {
+      localStorage.setItem("selectedTime", date.bookingTime.toISOString());
+      router.push("/menu");
     }
+  }, [date.bookingTime]);
 
-    return bookingTimes;
-  }
-
-  const times = getBookingTimes();
+  const times = date.bookingDate && getOpeningTimes(date.bookingDate, days);
 
   function chooseBookingDate(date: Date) {
     setDate((prev) => ({ ...prev, bookingDate: date }));
@@ -53,6 +61,7 @@ const Calendar: FC<indexProps> = ({ date, setDate }: indexProps) => {
         <ReactCalendar
           className="REACT-CALENDAR p-2"
           view="month"
+          tileDisabled={({ date }) => closedDays.includes(formatISO(date))}
           onClickDay={chooseBookingDate}
           minDate={new Date()}
           locale="en-GB"
